773fb84a4ad6044035916d9d96af330b6057e786
Jim Hopkinson <jahopki@hotmail.com>
2020-07-30 18:27:37 -0600

    database models and associations from given data

--- models/index.js
+++ models/index.js
@@ -6,11 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'user_id',
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'user_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  foreignKey: 'user_id',
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  foreignKey: 'user_id',
+});
 
 module.exports = {
   Product,
