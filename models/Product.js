56a006a2b7344dd6c0802f2f463b3418a7ab9e17
Jim Hopkinson <jahopki@hotmail.com>
2020-07-31 00:08:35 -0600

    routes updated almost done

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,8 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -7,51 +10,48 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      //defaultValue: 10????????????????????????
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Category',
-        key: 'id'
-      }
-      //references the "category" model's "id" ?????????????
+Product.init({
+  // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true
     }
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    //defaultValue: 10????????????????????????
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Category',
+      key: 'id'
+    }
+    //references the "category" model's "id" ?????????????
   }
-);
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+});
 
 module.exports = Product;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,42 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Product',
-        key: 'id'
-      }
-      //References the product model's id????????????????????????????????????????/
-    },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Tag',
-        key: 'id'
-      }
-      //References the tag model's id??????????????????????????????????????????
-    }
+ProductTag.init({
+  // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Product',
+      key: 'id'
+    }
+    //References the product model's id????????????????????????????????????????/
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Tag',
+      key: 'id'
+    }
+    //References the tag model's id??????????????????????????????????????????
   }
-);
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product_tag',
+});
 
 module.exports = ProductTag;
